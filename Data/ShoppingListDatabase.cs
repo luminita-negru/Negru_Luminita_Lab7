af9f640749a84602d8eceb52f689426123d6767a
LUMINIȚA NEGRU <luminita.negru@stud.ubbcluj.ro>
2023-12-17 01:43:58 +0200

    Show on Map

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -18,6 +18,7 @@ namespace Negru_Luminita_Lab7.Data
             _database.CreateTableAsync<ShopList>().Wait();
             _database.CreateTableAsync<Product>().Wait();
             _database.CreateTableAsync<ListProduct>().Wait();
+            _database.CreateTableAsync<Shop>().Wait();
         }
 
         public Task<List<ShopList>> GetShopListsAsync()
@@ -88,6 +89,24 @@ namespace Negru_Luminita_Lab7.Data
                 shoplistid
             );
         }
+
+        public Task<List<Shop>> GetShopsAsync()
+        {
+            return _database.Table<Shop>().ToListAsync();
+        }
+
+        public Task<int> SaveShopAsync(Shop shop)
+        {
+            if (shop.ID != 0)
+            {
+                return _database.UpdateAsync(shop);
+            }
+            else
+            {
+                return _database.InsertAsync(shop);
+            }
+        }
+
 
     }
 }
--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
+
 namespace Negru_Luminita_Lab7.Models
 {
     public class ShopList {
@@ -11,6 +13,9 @@ namespace Negru_Luminita_Lab7.Models
         public int ID { get; set; }
         [MaxLength(250), Unique]
         public string Description { get; set; } 
-        public DateTime Date { get; set; } 
+        public DateTime Date { get; set; }
+
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
     }
 }
